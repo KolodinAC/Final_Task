30a3ac7bc529a940ea66a9ab422973d2133fd248
Anton Kolodin <anton.kolodin2012@yandex.ru>
2023-01-15 15:19:07 +0300

    task done

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,47 +1,58 @@
-﻿
-
-
-
-
-
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 
 string[] workArray = FillArray();
 string[] resultArray = GenerateNewArray(workArray);
-string firstArray = PrintArray(workArray);
-string secondArray = PrintArray(resultArray);
-Console.WriteLine(firstArray + " -> " + secondArray);
+string originalArray = PrintArray(workArray);
+string sortedArray = PrintArray(resultArray);
+
+Console.WriteLine("Ваш массив " + originalArray + " отсортирован: " + sortedArray);
+
+
+
+
+
+// Methods:
 
 string[] FillArray()
 {
-    Console.WriteLine("Введите данные через пробел, по окончании ввода нажмите Enter: ");
+    Console.WriteLine("Введите желаемые элементы массива через пробел, затем нажмите Enter: ");
+
     string? enterSymbols = Console.ReadLine();
-    if (enterSymbols == null) { enterSymbols = ""; };
+
+    if (enterSymbols == null) 
+    { 
+        enterSymbols = ""; 
+    };
+
     char[] separators = new char[] { ',', ' ' };
     string[] workArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
     return workArray;
 }
 
-string PrintArray(string[] workArray)
+string PrintArray(string[] array)
 {
     string stringArray = "[";
-    for (int i = 0; i < workArray.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if (i == workArray.Length - 1)
+        if (i == array.Length - 1)
         {
-            stringArray += $"\"{workArray[i]}\"";
+            stringArray += $"\"{array[i]}\"";
             break;
         }
-        stringArray += ($"\"{workArray[i]}\", ");
+        stringArray += ($"\"{array[i]}\", ");
     }
     stringArray += "]";
     return stringArray;
 }
 
-int CountStringSymbols(string[] workArray)
+int CountStringSymbols(string[] array)
 {
     int counter = 0;
-    foreach (string item in workArray)
+    foreach (string item in array)
     {
         if (item.Length <= 3)
         {
@@ -51,12 +62,12 @@ int CountStringSymbols(string[] workArray)
     return counter;
 }
 
-string[] GenerateNewArray(string[] workArray)
+string[] GenerateNewArray(string[] array)
 {
     int resultArrayLength = CountStringSymbols(workArray);
     string[] resultArray = new string[resultArrayLength];
     int i = 0;
-    foreach (string item in workArray)
+    foreach (string item in array)
     {
         if (item.Length <= 3)
         {
